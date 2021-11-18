078cd2deb062abf482044e441a11ced32ac2f86a
PcsaDev <omarsaidortiz09@gmail.com>
2021-11-18 12:37:09 -0600

    si

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,48 @@
-const three = require('three');
+class Planet {
 
-let earth = document.getElementById('earth-model')
+    putPlanet(nameId, pathPlanet) { 
+        let planet = document.getElementById(nameId);
+
+        var scene = new THREE.Scene()
+        scene.background = new THREE.Color('#3b4252');
+
+        var camera = new THREE.PerspectiveCamera(
+            50,
+            window.innerWidth/window.innerHeight
+        );
+        
+        var renderer = new THREE.WebGLRenderer({antialias:true});
+        renderer.setSize(700, 600);
+        
+        
+        planet.appendChild( renderer.domElement );
+        
+        var geometry = new THREE.BoxGeometry();
+        var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+        var cube = new THREE.Mesh(geometry, material);
+        
+        scene.add(cube);
+        
+        camera.position.z = 5;
+        let loader = new THREE.GLTFLoader();
+        loader.load('scene.glft', function(gltf) { 
+            scene.add(gltf.scene);
+            renderer.render(s)
+            renderer.render(scene,camera);
+        })
+
+        var animate = function() { 
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+        
+            //renderer.render(scene,camera);
+        }
+        
+        //animate();
+    }
+
+}
+
+let earth = new Planet();
+earth.putPlanet('earth', './asda')
