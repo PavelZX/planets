cfabdc8e0419f2444d2983d10622ed9222f6ae23
PcsaDev <omarsaidortiz09@gmail.com>
2021-11-18 13:24:32 -0600

    earth

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html')
 })
 
+app.get('/scene.gltf', function (req, res) {
+    res.sendFile(__dirname + '/public/scene.gltf')
+})
+
 app.listen(2021)
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 class Planet {
 
     putPlanet(nameId, pathPlanet) { 
@@ -7,39 +8,45 @@ class Planet {
         scene.background = new THREE.Color('#3b4252');
 
         var camera = new THREE.PerspectiveCamera(
-            50,
+            30,
             window.innerWidth/window.innerHeight
         );
-        
+        let hlight = new THREE.AmbientLight (0x404040,100);
+
+        scene.add(hlight);
         var renderer = new THREE.WebGLRenderer({antialias:true});
-        renderer.setSize(700, 600);
+        renderer.setSize(900, 500);
         
         
         planet.appendChild( renderer.domElement );
         
-        var geometry = new THREE.BoxGeometry();
-        var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-        var cube = new THREE.Mesh(geometry, material);
-        
-        scene.add(cube);
         
         camera.position.z = 5;
+        
+        let controls = new OrbitControls(camera,renderer.domElement);
+
+        controls.addEventListener('change', renderer);
+
+        
         let loader = new THREE.GLTFLoader();
-        loader.load('scene.glft', function(gltf) { 
+        loader.load('scene.gltf', function(gltf) { 
             scene.add(gltf.scene);
-            renderer.render(s)
+
             renderer.render(scene,camera);
+
+            animate();
+
         })
 
-        var animate = function() { 
+
+        function animate() {
+
+            renderer.render(scene,camera);
+    
             requestAnimationFrame(animate);
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-        
-            //renderer.render(scene,camera);
+    
         }
-        
-        //animate();
+    
     }
 
 }
